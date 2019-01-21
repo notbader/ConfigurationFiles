1f7ad6d0087a53b240520c72206d5769ba46cf43
notBader <bdrjalmal@gmail.com>
2019-01-21 21:31:11 +0300

    fixed symlink bug

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,64 @@
-C:/users/bdrja/OneDrive/Reference/config_files/Microsoft.PowerShell_profile.ps1
+# My Profile
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+# Modules -----------------------------------------------------------
+Import-Module posh-git
+Import-Module Get-ChildItemColor
+
+# Cosmetics ------------------------------------------------------
+$host.ui.RawUI.WindowTitle = "notBader" # Change title
+
+# Aliases ---------------------------------------------------------
+$SCRIPTPATH = "C:\Users\bdrja\WindowsPowerShell\Scripts" # Script path
+$VIMPATH    = "C:\users\bdrja\vim\vim81\vim.exe" # Vim path
+Set-Alias vi   $VIMPATH
+Set-Alias vim  $VIMPATH
+Set-Alias l Get-ChildItemColor -Option AllScope # Vertical list
+Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope # Horizental list
+New-Alias -name g -value gVim # Open gVim
+New-Alias ipconfig -value ip # IP
+New-Alias -name ipa -value ipconfig_all_function
+New-Alias -name x -value exit_function
+New-Alias -name home -value go_home
+New-Alias -name vp -value edit_profile
+
+# Functions ----------------------------------------------------------
+# ipconfig /all
+function ipconfig_all_function() {   
+    ipconfig /all
+}
+
+# Leave PowerShell
+function exit_function() {
+    exit
+}
+
+# Go home
+function go_home() {
+    cd $HOME
+} 
+
+# Easily edit $PROFILE
+function edit_profile() {
+   vim $PROFILE
+}
+
+# Change the prompt to show the date and only the current folder
+
+# function global:prompt {
+
+# Write-Host '[' -NoNewline
+# Write-Host (Get-Date -UFormat '%T') -ForegroundColor Blue -NoNewline
+# Write-Host ']: ' -NoNewline
+# Write-Host (Split-Path (Get-Location) -Leaf) -ForegroundColor Red -NoNewline # your current directory (only name of directory)
+# Write-host ' >' -Foregroundcolor yello -NoNewline
+# return " " 
+
+# }
+
+
